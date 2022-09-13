f7dd1f12c02aed5c559389fc35338cd8abadd87d
Jewoo <jeus0630@gmail.com>
2022-09-13 17:33:54 +0900

    Initial commit

---
+++ src/main/java/com/example/springfilter/controller/ApiController.java
@@ -0,0 +1,22 @@
+package com.example.springfilter.controller;
+
+import com.example.springfilter.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+public class ApiController {
+
+    @PostMapping
+    public User user(@RequestBody User user){
+        log.info("User : {}", user);
+        return user;
+    }
+
+    @GetMapping
+    public void get(){
+
+    }
+}
---
+++ src/main/java/com/example/springfilter/dto/User.java
@@ -0,0 +1,11 @@
+package com.example.springfilter.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
